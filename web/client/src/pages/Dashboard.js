74b6dfe2b39a040f3ce1013d61b4a9a92172acff
adel <86808805+elithaxxor@users.noreply.github.com>
2025-04-13 00:56:58 -0400

    Update Dashboard.js

--- web/client/src/pages/Dashboard.js
+++ web/client/src/pages/Dashboard.js
@@ -1,4 +1,4 @@
- import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import ScanForm from '../components/ScanForm';
 import ScheduleForm from '../components/ScheduleForm';
